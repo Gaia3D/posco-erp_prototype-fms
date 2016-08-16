4b4fcfbd2e9c42941286cdd01fdd28347c6f6230
jsKim <kimjs4384@gmail.com>
2016-08-16 11:46:42 +0900

    점검 상세 페이지 오류 수정

--- poscoFMS/src/main/webapp/common/js/controlForDetailedInspection.js
+++ poscoFMS/src/main/webapp/common/js/controlForDetailedInspection.js
@@ -4,6 +4,7 @@
 
 // AJAX 요청 성공 시 실행
 function refreshDetailPageToNewInspection(result){
+	$(".check_list>table tbody").empty();
 	if(result.girderId == null){
 		$(".check_list h3").html("점검 ( 교체이력 및 준공이력이 없습니다. )");
 		trTag = '<tr><td colspan="11">해당 거더의 준공 및 교체 이력이 없습니다.</td></tr>';
@@ -14,27 +15,26 @@ function refreshDetailPageToNewInspection(result){
 				"점검 ( 교체일 : "+ result.girderReplacementDate +", 교체이유 : "+ result.replacementReason + ", 담당자 : " + result.personInCharge + " )" );
 	}
 	
-	$(".check_list>table tbody").empty();
-	var trTag = ""
+	trTag = "";
 	inspectionDatas = result.inspectionRecords;
 	if(inspectionDatas && inspectionDatas.length == 0){
 		trTag = '<tr><td colspan="11">점검이력이 없습니다.</td></tr>';
 		$(".check_list>table").append(trTag);
 	}else{
 		for (inspectionRow in inspectionDatas){
-			inspectionData = inspectionDatas[inspectionRow]
-			trTag += "<tr>"
+			inspectionData = inspectionDatas[inspectionRow];
+			trTag += "<tr>";
 			for (categoryName in inspectionData){
-				data = inspectionData[categoryName]
+				data = inspectionData[categoryName];
 				if(data == 0){
-					data = '-'
-					trTag += "<td>"+ data
+					data = '-';
+					trTag += "<td>"+ data;
 				}else if(!isNaN(data) && categoryName != "inspectionType" && categoryName != "count"){
-					trTag += '<td class="warn">'+ data
+					trTag += '<td class="warn">'+ data;
 				}else{
-					trTag += "<td>"+ data
+					trTag += "<td>"+ data;
 				}
-				trTag += "</td>"
+				trTag += "</td>";
 			}
 			trTag += "</tr>";
 		}
