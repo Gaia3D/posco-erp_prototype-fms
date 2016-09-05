c924c1aaea90a0a6052c34bc6534ed55ad3dd35b
jsKim <kimjs4384@gmail.com>
2016-09-05 18:13:17 +0900

    점검 페이지 텍스트 수정

--- poscoFMS/src/main/webapp/common/js/controlForDetailedInspection.js
+++ poscoFMS/src/main/webapp/common/js/controlForDetailedInspection.js
@@ -6,8 +6,8 @@
 function refreshDetailPageToNewInspection(result){
 	$(".check_list>table tbody").empty();
 	if(result.girderId == null){
-		$(".check_list h3").html("점검 ( 교체이력 및 준공이력이 없습니다. )");
-		trTag = '<tr><td colspan="11">해당 거더의 준공 및 교체 이력이 없습니다.</td></tr>';
+		$(".check_list h3").html("점검 ( 1977년도 준공, 공장준공 )");
+		trTag = '<tr><td colspan="11">점검 이력이 없습니다.</td></tr>';
 		$(".check_list>table").append(trTag);
 		return;
 	}else{
